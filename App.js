81f654da4baf9a9b24dc3c99488de79a2c35db5b
HeroRickyGAMES <ricojn9@gmail.com>
2022-08-02 11:13:23 -0300

    Treinando alinhamento de componentes no React Native

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from "react";
+import {View, Text} from 'react-native';
+
+class App extends Component{
+
+  render(){
+    return(
+      <View style={{flex: 1, flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end'}}>
+        <View style={ { width: 50, height:50, backgroundColor: 'green' } }></View>
+        <View style={ { width: 50, height:50, backgroundColor: 'red' } }></View>
+        <View style={ { width: 50, height:50, backgroundColor: 'yellow' } }></View>
+      </View>
+    );
+  }
+}
+
+export default App
